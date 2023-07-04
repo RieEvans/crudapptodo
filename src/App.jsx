ac4e58375032fae45e4a7ff2608014214a7ecc7f
Riejan <riejanevangelista1620@gmail.com>
2023-07-04 22:57:45 +0800

    Crud/withDatabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react"
 import {Routes, Route} from 'react-router-dom'
 import { TodoWrapper } from "./components/TodoWrapper"
+import { TodoWrapperLocalStorage } from "./components/TodoWrapperLocalStorage"
 
 export default function App() {
   return (
     <div>
         <Routes>
-            <Route path="/" element = {<TodoWrapper />} />
+            <Route path="/" element = {<TodoWrapperLocalStorage />} />
         </Routes>
     </div>
   )
